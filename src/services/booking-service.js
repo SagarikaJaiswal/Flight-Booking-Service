65f254be434d193625050f23659681cd104e0171
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2026-07-17 12:56:26 +0530

    Added booking cancellation logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -52,7 +52,7 @@ async function makePayment(data) {
         const bookingTime = new Date(bookingDetails.createdAt);
         const currentTime = new Date();
         if(currentTime - bookingTime > 300000){
-            await bookingRepository.update(data.bookingId, {status: CANCELLED}, transaction);
+            await cancelBooking(data.bookingId);
             throw new AppError('The booking has expired', StatusCodes.BAD_REQUEST); 
         }
         if(bookingDetails.totalCost != data.totalCost){
@@ -69,6 +69,32 @@ async function makePayment(data) {
         await transaction.rollback();
         //console.log(error);
         throw new AppError(error, StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+ 
+/**
+ * create a cancel booking api which is called automatically every 5 min
+ * to check if the 5 min has passed since the booking is initiated and cancels it
+ * After cancelling we also have to increase the seats and update the bookingStatus in the gb
+ */
+
+async function cancelBooking(bookingId) {
+    const transaction = await db.sequelize.transaction();
+    try {
+        const bookingDetails = await bookingRepository.get(bookingId, transaction);
+        if(bookingDetails.status == CANCELLED){
+            await transaction.commit();
+            return true;
+        }
+        const response = await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`,{
+            seats: bookingDetails.noOfSeats,
+            dec: 0
+        });
+        await bookingRepository.update(bookingId, {status: CANCELLED}, transaction);
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
     }
 }
 module.exports = {
